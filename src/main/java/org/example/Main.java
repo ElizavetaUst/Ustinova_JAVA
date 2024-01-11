024be30e7cfb8c95f420c9d260e1c6976b222ce1
Elizaveta Ustinova <weta.esu@yandex.ru>
2024-01-11 07:20:32 +0300

    Задание 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int number = sc.nextInt();
-        if (number > 7) {
-            System.out.println("Привет");
+        System.out.println("Введите имя: ");
+        String name = sc.nextLine();
+        if (name.equals("Вячеслав")) {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
         }
     }
 }
