d951ebfe04b4f7c363208ffad18638c2b732e553
Elizaveta Ustinova <weta.esu@yandex.ru>
2024-01-11 08:03:03 +0300

    Задание 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите имя: ");
-        String name = sc.nextLine();
-        if (name.equals("Вячеслав")) {
-            System.out.println("Привет, Вячеслав");
-        } else {
-            System.out.println("Нет такого имени");
+        System.out.println("Введите размер массива: ");
+       int size = sc.nextInt();
+       int[] arr = new int[size];
+       System.out.println("Введиет элементы массива: " );
+       for (int i = 0; i < size; i++) {
+           arr[i] = sc.nextInt();
+       }
+       System.out.println("Вывод элементов кратных 3: ");
+       for (int i = 0; i < size; i++) {
+           if (arr[i] % 3 == 0) {
+               System.out.print(arr[i]);
+           }
         }
     }
 }
